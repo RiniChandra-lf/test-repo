c014acf543a6872f3ee85afd0cf387012b9a0dc6
RiniChandra-lf <rini@localfactor.ai>
2025-07-02 16:58:47 +0530

    quart events 2

--- template4_expt.js
+++ template4_expt.js
@@ -92,7 +92,7 @@ const VpaidNonLinear = class {
      */
     this.quartileEvents_ = [
       //{ event: "AdImpression", value: 0 },
-      //{ event: "AdVideoStart", value: 0 },
+      { event: "AdVideoStart", value: 0 },
       { event: "AdVideoFirstQuartile", value: 25 },
       { event: "AdVideoMidpoint", value: 50 },
       { event: "AdVideoThirdQuartile", value: 75 },
